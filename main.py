ed12f89017ed43cd90755618f688af2af27ac4b3
Tenco <theancientjammer@gmail.com>
2025-04-13 15:17:17 +0000

    Update main.py, .gitignore

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import pygame
 from constants import * 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    
+    while True:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        screen = pygame.display.set_mode((SCREEN_HEIGHT,SCREEN_WIDTH))
+        screen.fill('black')
+    
+        pygame.display.flip()
     
 if __name__ ==  "__main__":
     main()
